7e754acfd2dac6c6f56844cb6701527902dd4611
hamza <rhirha.hamza@gmail.com>
2023-08-15 16:36:55 +0100

    Initial commit

---
+++ level09/resources/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main()
+{
+	int fd, len;
+	char s[1024];
+	if ((fd = open("token", O_RDONLY)) < 0)
+		return 1;
+
+	len = read(fd, s, 1024);
+	s[len] = '\0';
+	for (unsigned int i = 0; i < len; i++)
+	{
+		printf("%c", s[i]-i);
+	}
+	printf("\n");
+	return 0;
+}
